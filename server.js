0f4c083ad997c4fd3e04f70fb4609d46eab95644
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-02 22:02:35 +0300

    sever

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 1234
 
-app.get('/', (req, res) => {
-    res.send('<h1>AAAAAAAAAAAAAA</h1>')
-})
+app.use(express.static("public"))
+
 
 app.listen(PORT, () => {
     console.log(`listner on http://localhost:${PORT}`)

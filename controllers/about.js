80b6c7efffb588ed39b685ccda1571e4c337b0d4
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-07 19:22:55 +0300

    data

--- controllers/about.js
+++ controllers/about.js
@@ -1,5 +1,10 @@
+// const { about } = require('../data/about.json')
+// const data = JSON.stringify(about)
+// console.log(about)
+// console.log(data)
 exports.about = async (req, res) => {
     res.render('pages/about', {
-        title: "About page"
+        title: "About page",
+        // about
     })
 }
